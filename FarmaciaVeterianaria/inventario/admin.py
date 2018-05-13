8424949e3b61cb624bcc6db6a9434c2d0285b3e7
diegoasencio96 <diegoasencio96@gmail.com>
2018-05-13 14:53:35 -0500

    Primeros modelos

--- FarmaciaVeterianaria/inventario/admin.py
+++ FarmaciaVeterianaria/inventario/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import *
+from forms import *
 
 # Register your models here.
+
+admin.site.register(Paciente)
+admin.site.register(Historial)
+admin.site.register(Procedimiento)
+admin.site.register(TipoMedicamento)
+admin.site.register(Producto)
